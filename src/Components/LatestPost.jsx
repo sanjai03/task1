b68ca56beea54651a244fb37ad887c003a0478b8
unknown <iveejnas45@gmail.com>
2021-01-28 18:28:15 +0530

    replace return by set bracket in map function

--- src/Components/LatestPost.jsx
+++ src/Components/LatestPost.jsx
@@ -9,13 +9,14 @@ function LatestPost(props){
         <div className="col-10"><h4 className="text-success">Recent News</h4><h2 className="align-left">Our Latest News</h2></div>
         <div className="col-2"><button className="button1">view more</button></div>
         {
-            props.posts.map((post)=>{
-                return(
-                    <div key={post.id} className="col-4 mb-3">
+            props.posts.map((post)=>
+                
+                   ( <div key={post.id} className="col-4 mb-3">
                         <NewsCard name={post.title} id={post.id} desc={post.body} />
                     </div>
-                )
-            })
+                   )
+                
+            )
         }
         
         </div>
