81c45d678a2416c09d3f608b23095ff54a108c06
unknown <iveejnas45@gmail.com>
2021-01-28 15:24:37 +0530

    it created for show all post

---
+++ src/Components/LatestPost.jsx
@@ -0,0 +1,25 @@
+import NewsCard from './PostCard'
+import image from '../image/nutrion.jpg'
+function LatestPost(props){
+    
+    return(
+        <div className="row my-3">
+        <div className="col-1"></div>
+        <div className ="col-10 mb-3 row">
+        <div className="col-10"><h4 className="text-success">Recent News</h4><h2 className="align-left">Our Latest News</h2></div>
+        <div className="col-2"><button className="button1">view more</button></div>
+        {
+            props.posts.map((post)=>{
+                return(
+                    <div key={post.id} className="col-4 mb-3">
+                        <NewsCard name={post.title} id={post.id} desc={post.body} />
+                    </div>
+                )
+            })
+        }
+        
+        </div>
+</div>
+    )
+}
+export default LatestPost
