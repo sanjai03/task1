9abb4f7fa34201ff0ff0c23fbf4a54346d5855e1
unknown <iveejnas45@gmail.com>
2021-01-28 15:16:19 +0530

    it is shows single post detail

---
+++ src/view/Post.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function Post() {
+    const[post,setPost]=useState({})
+    let {id} = useParams()
+    const url = "https://jsonplaceholder.typicode.com/posts/"+id;
+
+     useEffect(()=>{
+        axios.get(url)
+      .then((res)=>{
+    
+        setPost(res.data)
+      })
+     },[])
+    
+return(
+<div className="p-3"><h1>{post.title}</h1>
+<p>{post.body}</p>
+</div>
+)
+    
+}
+export default Post
